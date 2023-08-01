81657e2553483a3fd8abb0e725962e920f4d0146
wangkp <wangkp007@foxmail.com>
2023-08-02 00:58:04 +0800

    Mpaas Demo

---
+++ app/src/main/java/com/www/mpaasdemo/MainApplication.java
@@ -0,0 +1,12 @@
+package com.www.mpaasdemo;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
