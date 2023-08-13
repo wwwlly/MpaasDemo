923733dfd7b2545d97971b1fc29cb49082f83b46
wangkp <wangkp007@foxmail.com>
2023-08-13 22:32:16 +0800

    mpaas build

--- app/src/main/java/com/mpaas/demo/MainApplication.java
+++ app/src/main/java/com/mpaas/demo/MainApplication.java
@@ -1,12 +1,29 @@
 package com.mpaas.demo;
 
 import android.app.Application;
+import android.content.Context;
+
+//import com.alipay.mobile.framework.quinoxless.IInitCallback;
+//import com.alipay.mobile.framework.quinoxless.QuinoxlessFramework;
 
 public class MainApplication extends Application {
 
     @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        // mPaaS 初始化回调设置。
+//        QuinoxlessFramework.setup(this, new IInitCallback() {
+//            @Override
+//            public void onPostInit() {
+//                // 此回调表示 mPaaS 已经初始化完成，mPaaS 相关调用可在这个回调里进行。
+//            }
+//        });
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
-
+        // mPaaS 初始化。
+//        QuinoxlessFramework.init();
     }
 }
