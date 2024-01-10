e5ea997d05ee22bd9b416464ca0b9601cfcf442a
wangkp <wangkp007@foxmail.com>
2024-01-11 00:49:42 +0800

    mriver

--- app/src/main/java/com/mpaas/demo/ui/activity/MriverActivity.kt
+++ app/src/main/java/com/mpaas/demo/ui/activity/MriverActivity.kt
@@ -1,6 +1,8 @@
 package com.mpaas.demo.ui.activity
 
 import android.os.Bundle
+import android.view.View
+import com.mpaas.demo.R
 
 /**
  * 小程序
@@ -9,6 +11,12 @@ class MriverActivity:BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mriver)
+    }
+
+    fun onClickStart(view: View) {
 
     }
+
+    fun onClickDebug(view: View) {}
 }
